7ae1603d31a433ea1521a2c215903baa94bd9130
jrkaren9 <40191282+jrkaren9@users.noreply.github.com>
2022-04-17 01:47:50 -0300

    Change little details

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default  function Footer() {
     
     return (
         <>
+            <Row>
+                <Col>
+                
+                </Col>
+                
+            </Row>
         </>
     );
 }
--- src/components/ImgPreview.jsx
+++ src/components/ImgPreview.jsx
@@ -1,32 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import s from './Items/ItemDetail.module.css'
+import s from './Items/ItemDetail.module.css';
 
 export default function ImgPreview({ item }) {
-  return (
-    <>
-    <Container>
-        <Row className="justify-content-center">
-            <Col xs={true} className="d-flex flex-column">
-                {item.all_imgs.map((small_img, index) => (
-                    <React.Fragment key={small_img} >
-                        <img src={small_img} alt={"Imagen del producto: " + item.name} className={s.PreviewImage + " align-self-end"}/> 
-                    </React.Fragment>
-                ))}
-            </Col>
-            
-            <Col xs={8} md={4} className="align-self-start">
-                <img src={item.img} alt={"Imagen del producto: " + item.name} className={s.ImageShown + " justify-content-center"}/>
-            </Col>
+    const [imgURL, setImg] = useState(item.img);
 
-            <Col xs={{ size:6, order: 'first' }} sm={{ order: 'last'}} md={4} >
-                <h2>{item.name}</h2>
-                <p>{item.text}</p>
-            </Col>    
-        </Row>  
-    </Container>
-    </>
-  )
+    const handleClick = (img) => {
+        setImg(img);
+    }
+
+    return (
+        <>
+        <Container>
+            <Row className="justify-content-center">
+                <Col xs={true} className="d-flex flex-column">
+                    {item.all_imgs.map((small_img, index) => (
+                        <React.Fragment key={small_img} >
+                            <img src={small_img} alt={"Imagen del producto: " + item.name} className={s.PreviewImage + " align-self-end"} onClick={ () => {handleClick(small_img)} }/> 
+                        </React.Fragment>
+                    ))}
+                </Col>
+                
+                <Col xs={8} md={4} className="align-self-start">
+                    <img src={imgURL} alt={"Imagen del producto: " + item.name} className={s.ImageShown + " justify-content-center"}/>
+                </Col>
+
+                <Col xs={{ size:6, order: 'first' }} sm={{ order: 'last'}} md={4} >
+                    <h2>{item.name}</h2>
+                    <p>{item.text}</p>
+                </Col>    
+            </Row>  
+        </Container>
+        </>
+    )
 }
