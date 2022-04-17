6559e222678ae18e86743b40de44840d9936efa6
jrkaren9 <40191282+jrkaren9@users.noreply.github.com>
2022-04-16 22:03:36 -0300

    Give style to item detail

--- src/components/Items/ItemDetail.jsx
+++ src/components/Items/ItemDetail.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ImgPreview from '../ImgPreview';
 import ItemCount from './ItemCount';
 import s from './ItemDetail.module.css'
 
@@ -8,27 +10,19 @@ import s from './ItemDetail.module.css'
 export default  function ItemDetail({ item, onAdd }) {
     return (
         <>
-        <Row id={s.ItemDetail}>
-            <Col xs={12} sm={8} id={s.MainInfo}>
-                <h2>{item.name}</h2>
-                <Row>
-                    <Col xs={3}>
-                    
-                    </Col>
-                    
-                    <Col xs={9} className="d-flex justify-content-start">
-                        <img src={item.img} alt={"Imagen del producto: " + item.name} className={s.ImageShown}/>
-                    </Col>
-                </Row>
-                <p>{item.text}</p>
-            </Col>
-            
-            <Col xs={12} sm={4} id={s.BuySectionContainer}>
-                <div id={s.BuySection}>
-                    <ItemCount stock={item.stock} initial={item.stock > 0 ? 1 : 0} onAdd={onAdd} />
-                </div>
-            </Col>
-        </Row>
+        <Container id={s.ItemDetail}>
+            <Row className="justify-content-center"> 
+                <Col xs={12} sm={8} id={s.MainInfo}>
+                    <ImgPreview item={item}/>
+                </Col>
+                
+                <Col xs={12} sm={4} id={s.BuySectionContainer}>
+                    <div id={s.BuySection}>
+                        <ItemCount stock={item.stock} initial={item.stock > 0 ? 1 : 0} onAdd={onAdd} />
+                    </div>
+                </Col>
+            </Row>
+        </Container>
         </>
     );
 }
