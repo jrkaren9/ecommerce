1572f52bf217669dab64393121429dfee51ede7e
Karen <40191282+jrkaren9@users.noreply.github.com>
2022-04-22 16:07:08 -0300

    Connect to database part 1

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCrFnMPFaCYz6ZZ9Fdk1NT1CG_qLfHKqhI",
+  authDomain: "venchibake.firebaseapp.com",
+  projectId: "venchibake",
+  storageBucket: "venchibake.appspot.com",
+  messagingSenderId: "307865694231",
+  appId: "1:307865694231:web:5ab666acbcef9c83dbec52",
+  measurementId: "G-419YM2XL1X"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
